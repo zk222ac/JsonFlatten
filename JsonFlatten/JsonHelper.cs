2f66e538cc6e1f131364bee9625137a62c942cf3
ajh <84565243+ajhNexcom@users.noreply.github.com>
2021-09-24 11:19:55 +0200

    Add project files.

---
+++ JsonFlatten/JsonHelper.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using NJsonSchema.CodeGeneration.CSharp;
+using JsonSchema = NJsonSchema.JsonSchema;
+
+namespace JsonFlatten
+{
+    public class JsonHelper
+    {
+        public static Dictionary<string, object> DeserializeAndFlatten(string json)
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            JToken token = JToken.Parse(json);
+            FillDictionaryFromJToken(dict, token, "");
+            return dict;
+        }
+
+        private static void FillDictionaryFromJToken(Dictionary<string, object> dict, JToken token, string prefix)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    foreach (JProperty prop in token.Children<JProperty>())
+                    {
+                        FillDictionaryFromJToken(dict, prop.Value, Join(prefix, prop.Name));
+                    }
+                    break;
+
+                case JTokenType.Array:
+                    int index = 1;
+                    foreach (JToken value in token.Children())
+                    {
+                        string nPrefix = Join(prefix, index.ToString());
+                        FillDictionaryFromJToken(dict, value, nPrefix);
+                        index++;
+                    }
+                    break;
+
+                default:
+                    dict.Add(prefix, ((JValue)token).Value);
+                    break;
+            }
+        }
+
+        private static string Join(string prefix, string name)
+        {
+            return (string.IsNullOrEmpty(prefix) ? name : prefix + "_" + name);
+        }
+
+        public static Dictionary<string, object> TrimArrayValuesInDictionary(Dictionary<string, object> oDictioanry)
+        {
+            StringBuilder sbValue = new StringBuilder();
+            Dictionary<string, object> nDictionary = new Dictionary<string, object>();
+            for (int i = 0; i < oDictioanry.Count - 1; i++)
+            {
+                var currentElement = oDictioanry.ElementAt(i);
+                var nextElement = oDictioanry.ElementAt(i + 1);
+
+                var cKey = currentElement.Key;
+                var cValue = currentElement.Value;
+                var nKey = nextElement.Key;
+                var nValue = nextElement.Value;
+                // check if key has a last character is Digit
+                if (char.IsDigit(cKey[^1]) && cKey[^2] == '_')
+                {
+                    if (char.IsDigit(cKey[^1]) && char.IsDigit(nKey[^1]) && cKey[^2] == '_' && nKey[^2] == '_')
+                    {
+                        sbValue.Append(cValue + ",");
+                        if (!oDictioanry.ContainsKey(cKey)) continue;
+                        oDictioanry.Remove(cKey);
+                        i--;
+                    }
+                    else
+                    {
+                        sbValue.Append(cValue);
+                        var mKey = cKey[..^2];
+                        nDictionary.TryAdd(mKey, sbValue.ToString());
+                        sbValue.Clear();
+                    }
+                }
+                else
+                {
+                    nDictionary.TryAdd(cKey, cValue);
+                }
+
+            }
+            return nDictionary;
+        }
+
+
+        public static bool IsValidJson(string input)
+        {
+            input = input.Trim();
+            try
+            {
+                // check if input is not null
+                if (string.IsNullOrWhiteSpace(input)) { return false; }
+                if ((input.StartsWith("{") && input.EndsWith("}")) || (input.StartsWith("[") && input.EndsWith("]"))) 
+                {
+                    try
+                    {
+                        // Parse as a Token
+                        var jToken = JToken.Parse(input);
+                        if (jToken.Type == JTokenType.Object)
+                        {
+                            //parse the input into a JObject
+                            var jObject = JObject.Parse(input);
+                            foreach (var jo in jObject)
+                            {
+                                var name = jo.Key;
+                                JToken value = jo.Value;
+                                switch (value)
+                                {
+                                    //if the element has a missing value, it will be Undefined - this is invalid
+                                    case {Type: JTokenType.Undefined}:
+                                        Console.WriteLine($"Undefined Value is not acceptable - {name}-{value}");
+                                        return false;
+                                    case {Type: JTokenType.Null}:
+                                        Console.WriteLine($"Null Value is not acceptable- - {name}-{value}");
+                                        return false;
+                                }
+                            }
+                        }
+
+                    }
+                    catch (JsonReaderException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        return false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool IsStrictComplainceParse(string json)
+        {
+            try
+            {
+                // Throw an exception if the json string is not in strict compliance with the JSON standard by tokenize it with the JSON reader used by DataContractJsonSerializer:
+                using var stream = GenerateStreamFromString(json);
+                using var reader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+                while (reader.Read())
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                // Wrap the XmlException in a JsonReaderException
+                throw new JsonReaderException($"Invalid property identifier character is not acceptable, only double quote acceptable:{ex}");
+            }
+            // Then actually parse with Json.NET
+             JToken.Parse(json);
+             return true;
+        }
+
+        static MemoryStream GenerateStreamFromString(string value)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
+        }
+
+
+
+        //public static async Task<bool> IsCheckMultipleErrors(string jsonString)
+        //{
+        //    //JsonSchema schemaFromFile = null;
+        //    try
+        //    {
+
+        //        //var settings = new JsonSerializerSettings { Error = (se, ev) => { ev.ErrorContext.Handled = true; } };
+        //        //dynamic d = JsonConvert.DeserializeObject(jsonString, settings);
+
+        //        //dynamic d = JsonConvert.DeserializeObject(jsonString);
+        //        //schemaFromFile = JsonSchema.FromSampleJson(jsonString);
+        //        //var classGenerator = new CSharpGenerator(schemaFromFile, new CSharpGeneratorSettings
+        //        //{
+        //        //    ClassStyle = CSharpClassStyle.Poco,
+        //        //});
+        //        //var codeFile = classGenerator.GenerateFile();
+
+        //        //var schema = JsonSchema.FromType<typeof()>();
+        //        //var schemaData = schema.ToJson();
+        //        // var errors = schema.Validate(jsonString);
+        //        //loading schema from a file
+        //        // schemaFromFile = JsonSchema.FromSampleJson(jsonString);
+        //        //var errors = schemaFromFile.ToJson();
+        //        //Validating the json file
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        //var errors = schemaFromFile.ToJson();
+        //    }
+
+        //    return true;
+        //}
+
+
+
+
+    }
+}
---
+++ JsonFlatten/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonFlatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Valid Json Files - Successful Criteria 
+            string rawJson1 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw1.json";
+            string rawJson2 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw2.json";
+            string rawJson3 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw3.json";
+            string rawJson4 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw4.json";
+            string rawJson5 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw5.json";
+            string rawJson6 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw6.json";
+            string rawJson7 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw7_500_records.json";
+            string rawJson8 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw8_1000_records_array.json";
+            string rawJson9 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw9.json";
+            string rawJson10 = @"C:\Users\ajh\Desktop\Task Json Converter\Success Criteria\Raw10.json";
+
+
+            List<string> lsFiles = new List<string>()
+            {
+               rawJson1,rawJson2,rawJson3,rawJson4,rawJson5,rawJson6,rawJson7,rawJson8,rawJson9,rawJson10
+            };
+
+            // Invalid Json Files - Unsuccessful Criteria
+            string errorJson1 = @"C:\Users\ajh\Desktop\Task Json Converter\unsuccessful criteria\ErrorRaw1.json";
+            string errorJson2 = @"C:\Users\ajh\Desktop\Task Json Converter\unsuccessful criteria\ErrorRaw2.json";
+            string errorJson3 = @"C:\Users\ajh\Desktop\Task Json Converter\unsuccessful criteria\ErrorRaw3.json";
+            string errorJson4 = @"C:\Users\ajh\Desktop\Task Json Converter\unsuccessful criteria\ErrorRaw4.json";
+            string errorJson5 = @"C:\Users\ajh\Desktop\Task Json Converter\unsuccessful criteria\ErrorRaw5.json";
+           
+
+
+
+            string cJsonString = errorJson5;
+
+            try
+            {
+                if (File.Exists(cJsonString))
+                {
+                    // Reading data form File 
+                    using var sr = new StreamReader(cJsonString);
+                    var rawJsonString = sr.ReadToEnd();
+                    // Is  check multiple errors in a json file
+                    // Task<bool> task = JsonHelper.IsCheckMultipleErrors(rawJsonString);
+                    // Validate json file
+                    if (JsonHelper.IsValidJson(rawJsonString) && JsonHelper.IsStrictComplainceParse(rawJsonString))
+                    {
+                        var flattenTheJsonJsonString = MainFlattenTheJson(rawJsonString);
+                       // Console.WriteLine(flattenTheJsonJsonString);
+                       // Save data into file 
+                      _ = WriteDataIntoFile(flattenTheJsonJsonString);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Json String is Invalid");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+       public static string MainFlattenTheJson(string rawJsonString)
+        {
+            // flatten the json  
+            var oDict = JsonHelper.DeserializeAndFlatten(rawJsonString);
+            // Trim Arrays in comma separated values
+            var nDict = JsonHelper.TrimArrayValuesInDictionary(oDict);
+            Console.WriteLine("..................................................................................................");
+            // serialize json file here  
+            var translatorJsonString = JsonConvert.SerializeObject(nDict , Formatting.Indented);
+            foreach (var kvp in nDict)
+            {
+                Console.WriteLine(kvp.Key + ": " + kvp.Value);
+            }
+            return translatorJsonString;
+        }
+
+       static async Task WriteDataIntoFile(string jsonString)
+       {
+           // Set a variable to the Documents path.
+           string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+           // Write the specified text asynchronously to a new file named "WriteTextAsync.txt".
+           await using StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "Translator.json"));
+           await outputFile.WriteAsync(jsonString);
+           // : kindly check the translator file in this folder of your computer ( C: \Users\ajh\Documents)
+           // ajh --> probably your user name 
+       }
+    }
+}
